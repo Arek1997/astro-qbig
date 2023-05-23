e9499e04c7d8f7752ed853456355c7dffead2779
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2023-05-23 12:56:20 +0200

    Add astro, tailwind config, add css variable

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,32 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.astro'],
-	theme: {
-		extend: {
-			colors: {
-				red: 'var(--red)',
-			},
+  content: ["./src/**/*.astro"],
+  theme: {
+    extend: {
+      colors: {
+        red: "#a40001",
+      },
 
-			fontFamily: {
-				sansSerifSecondary: 'Poppins',
-			},
-		},
-		container: {
-			center: true,
-			padding: {
-				DEFAULT: '1rem',
-			},
-		},
-		fontFamily: {
-			sans: ['Roboto', 'sans-serif'],
-		},
+      backgroundImage: {
+        bgRedImage: "var(--background-red-image)",
+        bgGridImage: "var(--background-grid-image)",
+      },
 
-		screens: {
-			sm: '576px',
-			md: '768px',
-			lg: '992px',
-			xl: '1200px',
-			'2xl': '1440px',
-		},
-	},
+      fontFamily: {
+        sansSerifSecondary: "Poppins",
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+      },
+    },
+    fontFamily: {
+      sans: ["Roboto", "sans-serif"],
+    },
+
+    screens: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+      "2xl": "1440px",
+    },
+  },
 };
